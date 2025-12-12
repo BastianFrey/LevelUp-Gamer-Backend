6ee1af4ad6334393c0c832235c35f26d1580182f
bafrey <ba.frey@duocuc.cl>
2025-12-12 18:02:10 -0300

    arreglo

--- LevelUpGamer-Backend-main/src/main/java/com/levelupgamer/products/ProductoRepository.java
+++ LevelUpGamer-Backend-main/src/main/java/com/levelupgamer/products/ProductoRepository.java
@@ -1,8 +1,11 @@
 package com.levelupgamer.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
     Optional<Producto> findByCodigo(String codigo);
     boolean existsByCodigo(String codigo);
